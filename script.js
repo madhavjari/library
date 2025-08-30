136678e8e9742b5cc5ddb8492de0a3cb3fb985db
madhavjari <213913746+madhavjari@users.noreply.github.com>
2025-08-30 17:15:08 +0530

    created dialog, remove button functioning

--- script.js
+++ script.js
@@ -1,29 +1,13 @@
-// creating display element and appending in main container
-const container = document.querySelector('.container');
-const bookDisplay = document.createElement('div');
-bookDisplay.classList.add('display');
-container.appendChild(bookDisplay);
-container.insertBefore(bookDisplay,container.children[0]);
-
-//creating button element and appending in main container
-const addButton = document.createElement('button');
-addButton.textContent = 'Add books';
-container.appendChild(addButton);
-container.insertBefore(addButton,container.children[1]);
-
-//creating elements for displaying books
-const library = document.createElement('h2');
-library.textContent = 'Library';
-bookDisplay.appendChild(library);
-
+const bookDisplay = document.querySelector('.display');
 const bookContainer = document.createElement('div');
 bookContainer.classList.add('book-container');
 bookDisplay.appendChild(bookContainer);
 
 //function for taking user input and displaying in card form
-function createBookCard(title,author,pages,issue){    
+function createBookCard(id,title,author,pages,issue,status){    
     const card = document.createElement('div');
     card.classList.add('card');
+    card.id = 'a'+id;
     bookContainer.appendChild(card);
     
     const bookTitle = document.createElement('h4');
@@ -41,44 +25,92 @@ function createBookCard(title,author,pages,issue){
     const bookIssue = document.createElement('h4');
     bookIssue.textContent = 'Issue Year: '+ issue;
     card.appendChild(bookIssue);
+
+    const statusButton = document.createElement('button');
+    card.appendChild(statusButton);
+
+    const bookStatus = document.createElement('h4');
+    bookStatus.textContent = status;
+    statusButton.appendChild(bookStatus);
+
+    const removeButton = document.createElement('button');
+    removeButton.id = id;
+    card.appendChild(removeButton);
+
+    const bookRemove = document.createElement('h4');
+    bookRemove.textContent = 'Remove';
+    removeButton.appendChild(bookRemove);
+    
+    removeButton.addEventListener('click',() => {
+        const card = document.querySelector('#a'+id+'.card');
+        console.log(card);
+        if(card)card.remove();
+        const bookToRemove = id;
+        console.log(bookToRemove);
+        const index = myLibrary.findIndex(book => book.id === bookToRemove);
+        if(index !== -1)myLibrary.splice(index,1);
+    })
 }
+
 
 const myLibrary=[]; //for storing every book
 
 //constructor for books
-function Book(title,author,pages,issue){
-    this.id = crypto.randomUUID();
+function Book(id,title,author,pages,issue,status){
+    this.id = id;
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.issue = issue;
+    this.status = status;
 }
 
 //adding the user inputs in library array
-function addBookToLibrary(title,author,pages,issue){
-    const book = new Book(title,author,pages,issue);
+function addBookToLibrary(id,title,author,pages,issue,status){
+    const book = new Book(id,title,author,pages,issue,status);
     myLibrary.push(book);
     console.log(myLibrary);
-    createBookCard(title,author,pages,issue);
+    createBookCard(id,title,author,pages,issue,status);
 }
-
+const bookForm = document.getElementById('book-form');
 //form opens after clicking add button
-addButton.addEventListener('click',() => {
-    const form = document.querySelector('form');
-    form.classList.add('after-inputs');
+document.getElementById('add-book').addEventListener('click',() => {
+    bookForm.showModal();
 }
 )
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
+const pagesInput = document.getElementById('pages');
+const issueInput = document.getElementById('issue');
+const statusInput = document.getElementById('status');
+
+bookForm.addEventListener('close',() =>{
+    titleInput.setAttribute('required', 'true');
+    authorInput.setAttribute('required', 'true');
+    pagesInput.setAttribute('required', 'true');
+})
+
+document.getElementById('close-button').addEventListener('click',() =>{
+    titleInput.removeAttribute('required');
+    authorInput.removeAttribute('required'); 
+    pagesInput.removeAttribute('required');
+    bookForm.close();
+} )
 
 //taking user inputs and parsing the values
 form = document.querySelector('form');
 form.addEventListener('submit', function(event){
     event.preventDefault();
     const formElement = event.target;
-    const title = formElement.elements.title.value;
-    const author = document.getElementById('author').value;
-    const pages = document.getElementById('pages').value;
-    const issue = document.getElementById('issue').value;
-    addBookToLibrary(title,author,pages,issue);
+    const id = crypto.randomUUID();
+    const title = titleInput.value;
+    const author = authorInput.value;
+    const pages = pagesInput.value;
+    const issue = issueInput.value;
+    let status = statusInput.checked;
+    if (status) status = 'Read';
+    else status = 'Unread';
+    addBookToLibrary(id,title,author,pages,issue,status);
     formElement.reset();
 })
 
